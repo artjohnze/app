e221021ccd5ad20a0fd2434ae0d4f56069aa1818
Art John-Zelle Balubal <artjohn-zelle.balubal_cyn@isu.edu.ph>
2025-04-18 15:51:34 +0800

    git commit -m "addedd files"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+
+import { DUMMY_USERS } from './dummy-users'
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: false,
+  //imports: [HeaderComponent, UserComponent, TasksComponent ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'app';
+  users = DUMMY_USERS; // Dummy data from dummy-users.ts
+  selectedUserId? : string;
+
+  get selectedUser(){
+return this.users.find((user)=>user.id === this.selectedUserId)!;
+   }
+
+  onSelectUser(id: string){
+//console.log('Selected user: ' + id);
+this.selectedUserId = id;
+   }
 }
